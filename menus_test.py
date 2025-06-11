6a46d06bc9e031de5002c6d0d389c3532db484c6
Festivized <1zhangjoe@gmail.com>
2025-06-11 00:50:18 -0400

    25/05/11.1 restrucutring this to main() run was easy tbhon

--- menus_test.py
+++ menus_test.py
@@ -1,8 +1,7 @@
 import pygame
-from pygame.event import set_grab
-
+import random
 '''
-Todo
+Todo fuck no lmao
 - selective reblitting to improve performance
 - better structure
 - finish tutorial
@@ -17,11 +16,17 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 clock = pygame.time.Clock()
 
-# Constants Setup
+# constants setup
+# game
 GROUND_Y = 300  # The Y-coordinate of the ground level
 JUMP_GRAVITY_START_SPEED = -20  # The speed at which the player jumps
 
-if True:
+# variables init
+# menu
+menu_select = 0
+list_fontsize=[20,20,20]
+
+if True: # delete this iftrue when im on my final pass, ignore for now as its used to collapse the block
     # Assetloader
     # splashscreen
     splash_surf = pygame.image.load("assets/splash.png")
@@ -61,20 +66,10 @@ if True:
 pygame.display.set_icon(pygame.image.load("assets/graphics/egg/egg_1.png"))
 pygame.display.set_caption("Placeholder")
 
-# initial loop setup
-isrunning = True # initiates main loop
-isinmenu = True # enters menu load loop
-isingame = False # enters gameplay loop
-ismenuloaded = False # forces first menu load
-isgameloaded = False # forces game load
+# initial loop setup (changed from a whilerunning if loop to a system inspired by evan's modular stuff)
+menustate = 1 #[exit,menu,game,transition]
 
 # func defs
-def close():
-    # changes all runtime variables to False, exiting loop
-    global isrunning
-    global isinmenu
-    global ismenuloaded
-    isrunning = isinmenu = ismenuloaded = False
 def resize(index:int):
     '''scales text up to size if active'''
     global menu_select
@@ -85,179 +80,115 @@ def resize(index:int):
             list_fontsize[index]+=1
     elif 20 < list_fontsize[index]: # scale down if inactive
         list_fontsize[index]-=1
-    print(index,list_fontsize[index])
-# # load music
-# pygame.mixer.init()
-# pygame.mixer.music.load("assets/music/1.mp3")
+    # print(index,list_fontsize[index]) # for troubleshooting
+def animation_cycler(list: list,delmin:int,delmax:int):
+    '''cycles through anim frames for a sprite, set delmin=delmax if want consistant timing'''
 
-while isrunning:
-    while isinmenu:
-        while not ismenuloaded: # menu asset loader
-            # reloads variables
-            # invalid catcher
-            screen.fill("#B00BA5")
-            # menu status conditionals
-            ismenuinteract = False
-            isquitloaded = False
-            ismenuloaded = True
-            isinmenu =True
-            # variable initialization
-            menu_select = 0
-            quit_select = True
+def menuactive():
+    global menu_select
+    # reloads variables
+    # invalid catcher
+    screen.fill("#B00BA5")
+    # variable initialization
+    menu_select = 0
 
-            # font loader
-            title_font = pygame.font.Font(pygame.font.get_default_font(),50)
-            # menu font
-            list_fontsize=[20,20,20]
-            # Title
-            
-            surf_title = title_font.render("Placeholder", True, "White")
-            rect_title = surf_title.get_rect(topleft=(50,75)) # topleft=(50,75)
-            # Menu rect init
-            print("menu loaded",ismenuloaded)
-            pygame.display.update()
-        while ismenuloaded:
-            for event in pygame.event.get():
-                # pygame.QUIT --> user clicked X to close your window
-                if event.type == pygame.QUIT:
-                    close()
-                if event.type == pygame.KEYDOWN:
-                    print("event",event)
-                    if event.key == pygame.K_w and menu_select > 0: #cycle up
-                        menu_select -= 1
-                        ismenu_updated = True
-                    if event.key == pygame.K_s and menu_select < 2: #cycle down
-                        menu_select += 1
-                        ismenu_updated = True
-                    if event.key == pygame.K_RETURN:
-                        print("kreturn")
-                        if menu_select == 0: # Play
-                            ismenuloaded = False
-                            isinmenu = False
-                            isingame = True
-                        elif menu_select == 1: # Settings
-                            close()
-                        else: #exit
-                            close()
-                    # # for troubleshooting
-                    # print("event key",event.key)
-                    # print("menu index",menu_select)
-            resize(0)
-            resize(1)
-            resize(2)
+    # font loader
+    title_font = pygame.font.Font(pygame.font.get_default_font(),50)
+    # Title
+    surf_title = title_font.render("Placeholder", True, "White")
+    rect_title = surf_title.get_rect(topleft=(50,75)) # topleft=(50,75)
+    # Menu rect init
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            # pygame.QUIT --> user clicked X to close your window
+            if event.type == pygame.QUIT:
+                close()
+            if event.type == pygame.KEYDOWN:
+                print("event",event)
+                if event.key == pygame.K_w and menu_select > 0: #cycle up
+                    menu_select -= 1
+                    ismenu_updated = True
+                if event.key == pygame.K_s and menu_select < 2: #cycle down
+                    menu_select += 1
+                    ismenu_updated = True
+                if event.key == pygame.K_RETURN:
+                    print("kreturn")
+                    if menu_select == 0: # Play
+                        return 2
+                    elif menu_select == 1: # Settings
+                        return 0
+                    else: #exit
+                        return 0
+                # # for troubleshooting
+                # print("event key",event.key)
+                # print("menu index",menu_select)
+        resize(0)
+        resize(1)
+        resize(2)
+        # menu renderer
+        clock.tick(100)
+        screen.fill("#B00BA5") #// holy shit calamity reference
+        menu_font1 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[0])
+        menu_font2 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[1])
+        menu_font3 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[2])
+        surf_1 = menu_font1.render("Play", True, "White")
+        surf_2 = menu_font2.render("Settings", True, "White")
+        surf_3 = menu_font3.render("Exit", True, "White")
+        rect_1 = surf_1.get_rect(midleft=(50,211)) # topleft=(50,200)
+        rect_2 = surf_2.get_rect(midleft=(50,261)) # topleft=(50,250)
+        rect_3 = surf_3.get_rect(midleft=(50,311)) # topleft=(50,300)
+        # screen.blit(surf_1,rect_1)
+        screen.blits(((splash_surf,splash_rect),(surf_title,rect_title),(surf_1,rect_1),(surf_2,rect_2),(surf_3,rect_3)))
+        # print(screen.blits(((surf_title,rect_title),(surf_1,rect_1),(surf_2,rect_2),(surf_3,rect_3)),doreturn=True)) #// selective reblitting? have all updated objects be placed in a tuple list?
+        pygame.display.flip()
+def gameactive():
+    global menustate
+    # Game state variables reinit
+    players_gravity_speed = 0  # The current speed at which the player falls
+    isalive = True
+    start_time = pygame.time.get_ticks()
 
-            # menu renderer
-            clock.tick(100)
-            screen.fill("#B00BA5") #// holy shit calamity reference
-            menu_font1 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[0])
-            menu_font2 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[1])
-            menu_font3 = pygame.font.Font(pygame.font.get_default_font(),list_fontsize[2])
-            surf_1 = menu_font1.render("Play", True, "White")
-            surf_2 = menu_font2.render("Settings", True, "White")
-            surf_3 = menu_font3.render("Exit", True, "White")
-            rect_1 = surf_1.get_rect(midleft=(50,211)) # topleft=(50,200)
-            rect_2 = surf_2.get_rect(midleft=(50,261)) # topleft=(50,250)
-            rect_3 = surf_3.get_rect(midleft=(50,311)) # topleft=(50,300)
-            # screen.blit(surf_1,rect_1)
-            screen.blits(((splash_surf,splash_rect),(surf_title,rect_title),(surf_1,rect_1),(surf_2,rect_2),(surf_3,rect_3)))
-            # print(screen.blits(((surf_title,rect_title),(surf_1,rect_1),(surf_2,rect_2),(surf_3,rect_3)),doreturn=True)) #// selective reblitting? have all updated objects be placed in a tuple list?
-            pygame.display.flip()
-    while isingame:
-        while not isgameloaded: # game (re)loader
-            # Game state variables
-            players_gravity_speed = 0  # The current speed at which the player falls
-            isalive = True
-            start_time = pygame.time.get_ticks()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menustate=0
+            if event.type == pygame.KEYDOWN:
+                if isalive:
+                    if event.key == pygame.K_SPACE: # make it a chargeup?
+                        players_gravity_speed = -20
+                if event.key == pygame.K_ESCAPE:
+                    return 1
 
-        while isgameloaded:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    close()
-                if event.type == pygame.KEYDOWN:
-                    if isalive:
-                        if event.key == pygame.K_SPACE: # make it a chargeup?
-                            players_gravity_speed = 60
+        # Adjust egg's horizontal location then blit it
+            egg_rect.x -= 5
+            if egg_rect.right <= 0:
+                egg_rect.left = 800
+            screen.blit(egg_surf, egg_rect)
+        # playerblitter
+            players_gravity_speed += 1
+            player_rect.y += players_gravity_speed
+            if player_rect.bottom > GROUND_Y:
+                player_rect.bottom = GROUND_Y
+            screen.blit(player_surf, player_rect)
+        # add a func for checking colision
+        # if egg_rect.colliderect(player_rect):
+        #         is_playing = False
 
-            # def animation_cycler(list: list,delay )
-            # renderer
-
-            #     for event in pygame.event.get():
-            #         # pygame.QUIT --> user clicked X to close your window
-            #         if event.type == pygame.QUIT:
-            #             close()
-            #         if event.type == pygame.KEYUP:
-            #             print("event",event)
-            #             if event.key == pygame.K_w and menu_select > 0: #cycle up
-            #                 menu_select -= 1
-            #                 ismenu_updated = True
-            #             if event.key == pygame.K_s and menu_select < 2: #cycle down
-            #                 menu_select += 1
-            #                 ismenu_updated = True
-            #             if event.key == pygame.K_RETURN:
-            #                 print("kreturn")
-            #                 if menu_select == 0: # Play
-            #                     ismenuloaded = False
-            #                     isinmenu = False
-            #                     isingame = True
-            #                 elif menu_select == 1: # Settings
-            #                     close()
-            #                 else: #exit
-            #                     close()
-            #             # for troubleshooting
-            #             # print("event key",event.key)
-            #             # print("menu index",menu_select)
-            #     for event in pygame.event.get():
-            #     # pygame.QUIT --> user clicked X to close your window
-            #     if event.type == pygame.QUIT:
-            #         running = False
-            #
-            #     elif is_playing:
-            #         # When player wants to jump by pressing SPACE
-            #         if (
-            #                 event.type == pygame.KEYDOWN
-            #                 and event.key == pygame.K_j
-            #                 or event.type == pygame.K_f
-            #         ) and player_rect.bottom >= GROUND_Y:
-            #             players_gravity_speed = JUMP_GRAVITY_START_SPEED
-            #     else:
-            #         # When player wants to play again by pressing SPACE
-            #         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            #             is_playing = True
-            #             egg_rect.left = 800
-            #
-            # if is_playing:
-            #     screen.fill("purple")  # Wipe the screen
-            #
-            #     # Blit the level assets
-            #     screen.blit(SKY_SURF, (0, 0))
-            #     screen.blit(GROUND_SURF, (0, GROUND_Y))
-            #     # pygame.draw.rect(screen, "#c0e8ec", score_rect)
-            #     # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
-            #     screen.blit(score_surf, score_rect)
-            #
-            #     # Adjust egg's horizontal location then blit it
-            #     egg_rect.x -= 5
-            #     if egg_rect.right <= 0:
-            #         egg_rect.left = 800
-            #     screen.blit(egg_surf, egg_rect)
-            #
-            #     # Adjust player's vertical location then blit it
-            #     players_gravity_speed += 1
-            #     player_rect.y += players_gravity_speed
-            #     if player_rect.bottom > GROUND_Y:
-            #         player_rect.bottom = GROUND_Y
-            #     screen.blit(player_surf, player_rect)
-            #
-            #     # When player collides with enemy, game ends
-            #     if egg_rect.colliderect(player_rect):
-            #         is_playing = False
-            #
-            # # When game is over, display game over message
-            # else:
-            #     screen.fill("black")
-            #
-            # # flip() the display to put your work on screen
-            # pygame.display.flip()
-            #
-            # clock.tick(100)  # Limits game loop to 60 FPS
-pygame.display.flip()
+        # clock.tick(100)
+        # # renderer
+        # screen.blit(SKY_SURF, (0, 0))
+        # screen.blit(GROUND_SURF, (0, GROUND_Y))
+            # pygame.draw.rect(screen, "#c0e8ec", score_rect)
+            # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+            screen.blit(score_surf, score_rect)
+def main():
+    global menustate
+    while menustate != 0:
+        if menustate == 1:
+            menustate = menuactive()
+        elif menustate == 2:
+            menustate = gameactive()
+    pygame.quit()
+main()
+# imms o fucking tired
